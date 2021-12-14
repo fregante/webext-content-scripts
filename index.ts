abb57e3031018b4d5e9aeeffd855a74f33a07c8d
Federico Brigante <me@fregante.com>
2021-12-14 19:01:38 +0700

    Add `executeScript` and `insertCSS` that work both on MV2 and MV3 (#7)

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import chromeP from 'webext-polyfill-kinda';
-import type {Manifest} from 'webextension-polyfill';
+import type {Manifest, ExtensionTypes} from 'webextension-polyfill';
+
+const gotScripting = typeof chrome === 'object' && 'scripting' in chrome;
 
 function castArray<A = unknown>(possibleArray: A | A[]): A[] {
 	if (Array.isArray(possibleArray)) {
@@ -24,7 +26,7 @@ export async function executeFunction<Fn extends (...args: any[]) => unknown>(
 		frameId: 0,
 	};
 
-	if ('scripting' in chrome) {
+	if (gotScripting) {
 		const [injection] = await chrome.scripting.executeScript({
 			target: {
 				tabId,
@@ -43,6 +45,104 @@ export async function executeFunction<Fn extends (...args: any[]) => unknown>(
 	}) as [ReturnType<Fn>];
 
 	return result;
+}
+
+function arrayOrUndefined<X>(value?: X): [X] | undefined {
+	return typeof value === 'undefined' ? undefined : [value];
+}
+
+interface InjectionDetails {
+	tabId: number;
+	frameId?: number;
+	matchAboutBlank?: boolean;
+	allFrames?: boolean;
+	runAt?: ExtensionTypes.RunAt;
+	files: string [] | Array<{
+		code: string;
+	} | {
+		file: string;
+	}>;
+}
+
+// eslint-disable-next-line @typescript-eslint/naming-convention -- It follows the native naming
+export function insertCSS({
+	tabId,
+	frameId,
+	files,
+	allFrames,
+	matchAboutBlank,
+	runAt,
+}: InjectionDetails): void {
+	for (let content of files) {
+		if (typeof content === 'string') {
+			content = {file: content};
+		}
+
+		if (gotScripting) {
+			void chrome.scripting.insertCSS({
+				target: {
+					tabId,
+					frameIds: arrayOrUndefined(frameId),
+					allFrames,
+				},
+				files: 'file' in content ? [content.file] : undefined,
+				css: 'code' in content ? content.code : undefined,
+			});
+		} else {
+			void chromeP.tabs.insertCSS(tabId, {
+				...content,
+				matchAboutBlank,
+				allFrames,
+				frameId,
+				runAt: runAt ?? 'document_start', // CSS should prefer `document_start` when unspecified
+			});
+		}
+	}
+}
+
+export async function executeScript({
+	tabId,
+	frameId,
+	files,
+	allFrames,
+	matchAboutBlank,
+	runAt,
+}: InjectionDetails): Promise<void> {
+	let lastInjection: Promise<unknown> | undefined;
+	for (let content of files) {
+		if (typeof content === 'string') {
+			content = {file: content};
+		}
+
+		if (gotScripting) {
+			if ('code' in content) {
+				throw new Error('chrome.scripting does not support injecting strings of `code`');
+			}
+
+			void chrome.scripting.executeScript({
+				target: {
+					tabId,
+					frameIds: arrayOrUndefined(frameId),
+					allFrames,
+				},
+				files: [content.file],
+			});
+		} else {
+			// Files are executed in order, but code isn’t, so it must wait the last script #31
+			if ('code' in content) {
+				// eslint-disable-next-line no-await-in-loop -- On purpose, to serialize injection
+				await lastInjection;
+			}
+
+			lastInjection = chromeP.tabs.executeScript(tabId, {
+				...content,
+				matchAboutBlank,
+				allFrames,
+				frameId,
+				runAt,
+			});
+		}
+	}
 }
 
 export async function injectContentScript(
