4c58c2cc39df194ce5f2c8692aa3a763c8aa1f58
Federico Brigante <me@fregante.com>
2021-12-14 19:07:50 +0700

    Lint

--- index.ts
+++ index.ts
@@ -74,7 +74,7 @@ interface InjectionDetails {
 // eslint-disable-next-line @typescript-eslint/naming-convention -- It follows the native naming
 export function insertCSS({
 	tabId,
-	frameId,∂
+	frameId,
 	files,
 	allFrames,
 	matchAboutBlank,
